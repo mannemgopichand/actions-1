424e40a8261585a3ebfcc513b51289a1865b737f
mannemgopichand <67426588+mannemgopichand@users.noreply.github.com>
2024-11-22 06:26:40 +0530

    Update generate-ascii.sh

--- generate-ascii.sh
+++ generate-ascii.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get install cowsay -y
 cowsay -f dragon "run for cover, I'm dragon... GRRR" >> dragon.txt
-gr
-ep -i "dragon" dragon.txt
+gruuop
+p -i "dragon" dragon.txt
 cat dragon.txt
 ls -lart
